95581fddfdc89a67c35b1a2536bffbd027bb6805
Galaxy <vanhnganha@gmail.com>
2020-04-18 21:06:16 +0700

    Initial Commit

---
+++ RemindThings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindThings
+//
+//  Created by NganHa on 4/18/20.
+//  Copyright © 2020 Galaxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
